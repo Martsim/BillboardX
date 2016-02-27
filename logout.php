9446481e2929e61f710ac3dcc0c1d8341d3d43a0
RaunoKa <rauno.kaiv@hotmail.com>
2016-02-27 15:56:18 +0200

    Create logout.php

---
+++ logout.php
@@ -0,0 +1,8 @@
+<?
+session_start();
+session_destroy();
+
+$host = $_SERVER["HTTP_HOST"];
+$path = rtrim(dirname($_SERVER["PHP_SELF"]), "/\\");
+header("Location: http://$host$path/index.php");
+?>
