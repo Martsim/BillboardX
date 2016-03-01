171a77f390ec5b1ba0a19f25bf6806687bc6cc41
RaunoKa <rauno.kaiv@hotmail.com>
2016-03-01 21:04:48 +0200

    Nüüd saab oma postitusi kustutada

---
+++ kustuta.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+include('connect.php');
+
+if(!empty($_GET["id"])){
+	$id = $_GET["id"];
+	$kasutaja= $_SESSION['kasutaja'];
+
+	//kui see postitus(id) on tõesti selle autori post
+	$sth = $pdo->prepare("SELECT * FROM postitus WHERE autor = :kasutaja and id = :id");
+    $sth->bindParam(':kasutaja', $kasutaja);
+    $sth->bindParam(':id', $id);
+    $sth->execute();
+
+    $tulemus = $sth->fetchAll();
+
+    if(count($tulemus) > 0){
+    	$sth = $pdo->prepare("DELETE FROM postitus WHERE id = :id");
+    	$sth->bindParam(':id', $id);
+    	$sth->execute();
+    }
+
+}
+//tagasi index.php-sse
+$host = $_SERVER["HTTP_HOST"];
+$path = rtrim(dirname($_SERVER["PHP_SELF"]), "/\\");
+header("Location: http://$host$path/index.php");
+
+?>
