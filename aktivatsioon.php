109d8b86d09b8994d30ca0ba0427cf4267b25715
RaunoKa <rauno.kaiv@hotmail.com>
2016-03-11 19:36:43 +0200

    fail, kuhu viib link meilil, aktiveerib kasutaja

---
+++ aktivatsioon.php
@@ -0,0 +1,24 @@
+<?php
+if(!empty($_GET['token'])){
+	include('connect.php');
+	$token = $_GET['token'];
+
+	$sth = $pdo->prepare("SELECT * FROM kasutaja WHERE token = :token");
+        $sth->bindParam(':token', $token);
+        $sth->execute();
+
+        $tulemus = $sth->fetchAll();
+        
+        if(count($tulemus) > 0){
+        	$sth = $pdo->prepare("UPDATE `infoorum_db`.`kasutaja` SET `aktiveeritud` = 1 WHERE token = :token;");
+        	$sth->bindParam(':token', $token);
+        	$sth->execute();
+        	
+        	
+        }
+
+}
+$host = $_SERVER["HTTP_HOST"];
+$path = rtrim(dirname($_SERVER["PHP_SELF"]), "/\\");
+header("Location: http://$host$path/index.php");
+?>
