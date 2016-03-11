3f5b551fb96ead14785282870022bef316a11f4b
RaunoKa <rauno.kaiv@hotmail.com>
2016-03-11 19:26:31 +0200

    postitused järjestatakse nüüd uusimast

--- lae_kategooria.php
+++ lae_kategooria.php
@@ -8,13 +8,11 @@
  */
 
 if(!empty($_POST["id"])){
-    //$session_start(); //siin peaks toimuma ka kontroll, et kasutaja enda postitustel oleks ka kustutamise nupp
-    //pigem peaks kustuta.php-s kontroll olema
     include('connect.php');
     $id = $_POST["id"];
 
     try {
-        $sth = $pdo->prepare("SELECT * FROM postitus WHERE kat_id = :id");
+        $sth = $pdo->prepare("SELECT * FROM postitus WHERE kat_id = :id ORDER BY loodud DESC");
         $sth->bindParam(':id', $id);
         $sth->execute();
         $sisud = $sth->fetchAll();
